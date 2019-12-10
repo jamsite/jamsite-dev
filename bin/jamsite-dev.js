73e2f5e519b09d7b6bb4bcfaebb94d50eee01562
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-10 14:12:54 +0100

    Inherit jamsite config

--- bin/jamsite-dev.js
+++ bin/jamsite-dev.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { micro } = require('@jamsite/jamsite')
 const bs = require('browser-sync').create()
-const config = require('@jamsite/jamsite/config')
+const config = require('../config')
 
 const {
   jamsiteDevHandler,
@@ -9,12 +9,12 @@ const {
 } = require('../jamsite-dev-handler')
 
 bs.init({
-  proxy: 'localhost:3030',
+  proxy: `localhost:${config.devPort}`,
   middleware: false,
   open: false,
   notify: false,
   port: config.port
 })
 
-micro(jamsiteDevHandler).listen(3030)
+micro(jamsiteDevHandler).listen(config.devPort)
 jamsite.on('update', () => bs.reload())
---
+++ config.js
@@ -0,0 +1,3 @@
+const config = require('@jamsite/jamsite/config')
+config.devPort = 3030
+module.exports = config
