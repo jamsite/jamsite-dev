b096d03b2aa52828769a2a9d1a12c56d4634f50d
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-11 09:19:15 +0100

    Fix test

--- test/render-error.js
+++ test/render-error.js
@@ -4,5 +4,5 @@ const renderError = require('../lib/render-error')
 test('renders error', (t) => {
   const msg = 'error message'
   const error = new Error(msg)
-  t.assert(renderError(error).startsWith(`<body><pre>Error: ${msg}\n`))
+  t.assert(renderError(error).startsWith(`Error: ${msg}\n`))
 })
