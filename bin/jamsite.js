d432f3aa6662bddd4b62b596650c1ceef784fe0f
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-28 10:06:15 +0100

    Autoload bundlers as bs middleware

--- bin/jamsite.js
+++ bin/jamsite.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env NODE_ENV=development node
 const { getRequestHandler, jamsiteServer } = require('@jamsite/jamsite')
 const bs = require('browser-sync').create()
-const devMiddleware = require('../lib/dev-middleware')
+const jamsiteLogMiddleware = require('../lib/jamsite-log-middleware')
+const autoloadBsMiddleware = require('../lib/autoload-bs-middleware')
 const JamsitePagesDev = require('../lib/jamsite-pages-dev')
 const config = require('../config')
 
-const jamsite = new JamsitePagesDev(config.jamsite)
-const jamsiteDevHandler = devMiddleware(getRequestHandler(jamsite))
+;(async function(){
+  const bsMiddleware = await autoloadBsMiddleware(config.jamsite)
 
-bs.init({
-  proxy: `${config.server.host}:${config.server.devPort}`,
-  middleware: false,
-  open: false,
-  notify: false,
-  port: config.server.port
-})
+  bs.init({
+    proxy: `${config.server.host}:${config.server.devPort}`,
+    middleware: bsMiddleware,
+    open: false,
+    notify: false,
+    port: config.server.port
+  })
 
-jamsiteServer(jamsiteDevHandler, {
-  port: config.server.devPort,
-  host: config.server.host
-}).on('close', () => bs.exit())
+  const jamsite = new JamsitePagesDev(config.jamsite)
+  const jamsiteDevHandler = jamsiteLogMiddleware(getRequestHandler(jamsite))
 
-jamsite.on('update', () => bs.reload())
+  jamsiteServer(jamsiteDevHandler, {
+    port: config.server.devPort,
+    host: config.server.host
+  }).on('close', () => bs.exit())
+
+  jamsite.on('update', () => bs.reload())
+})()
