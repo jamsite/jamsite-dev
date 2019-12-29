f9935b00efc48a70b59df0ebe53b0a91bd37363c
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-29 21:12:15 +0100

    Remove bs middleware autoloader

--- bin/jamsite.js
+++ bin/jamsite.js
@@ -2,28 +2,26 @@
 const { getRequestHandler, jamsiteServer } = require('@jamsite/jamsite')
 const bs = require('browser-sync').create()
 const jamsiteLogMiddleware = require('../lib/jamsite-log-middleware')
-const autoloadBsMiddleware = require('../lib/autoload-bs-middleware')
 const JamsitePagesDev = require('../lib/jamsite-pages-dev')
 const config = require('../config')
 
-;(async function () {
-  const bsMiddleware = await autoloadBsMiddleware(config.jamsite)
+bs.init({
+  proxy: `${config.server.host}:${config.server.devPort}`,
+  middleware: false,
+  open: false,
+  notify: false,
+  port: config.server.port
+})
 
-  bs.init({
-    proxy: `${config.server.host}:${config.server.devPort}`,
-    middleware: bsMiddleware,
-    open: false,
-    notify: false,
-    port: config.server.port
-  })
+const jamsite = new JamsitePagesDev(config.jamsite)
+jamsite.on('update', () => bs.reload())
 
-  const jamsite = new JamsitePagesDev(config.jamsite)
-  const jamsiteDevHandler = jamsiteLogMiddleware(getRequestHandler(jamsite))
+const jamsiteDevHandler = jamsiteLogMiddleware(getRequestHandler(jamsite))
 
-  jamsiteServer(jamsiteDevHandler, {
-    port: config.server.devPort,
-    host: config.server.host
-  }).on('close', () => bs.exit())
-
-  jamsite.on('update', () => bs.reload())
-})()
+jamsiteServer(jamsiteDevHandler, {
+  port: config.server.devPort,
+  host: config.server.host
+}).on('close', () => {
+  jamsite.unwatch()
+  bs.exit()
+})
