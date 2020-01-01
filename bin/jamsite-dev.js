b4eef12fe17766fe17080a2490234f9f75502548
Aleksandr Tsertkov <tsertkov@gmail.com>
2020-01-01 18:28:49 +0100

    Rename jamsite to jamsite-dev

---
+++ bin/jamsite-dev.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { micro, getRequestHandler } = require('@jamsite/jamsite')
+const bs = require('browser-sync').create()
+const devMiddleware = require('../lib/dev-middleware')
+const JamsitePagesDev = require('../lib/jamsite-pages-dev')
+const config = require('../config')
+
+const jamsite = new JamsitePagesDev(config)
+const jamsiteDevHandler = devMiddleware(getRequestHandler(jamsite))
+
+bs.init({
+  proxy: `${config.devHost}:${config.devPort}`,
+  middleware: false,
+  open: false,
+  notify: false,
+  port: config.port
+})
+
+micro(jamsiteDevHandler).listen(config.devPort, config.devHost)
+jamsite.on('update', () => bs.reload())
